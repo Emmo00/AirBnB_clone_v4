909ba7f89444c7fc202f1169f010662672eebf83
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-08-07 16:10:50 +0100

    Task 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -17,3 +17,11 @@ $(function () {
     $(".amenities h4").text(amenityList);
   });
 });
+$(function () {
+  $.get('http://0.0.0.0:5001/api/v1/status', function (data, status) {
+	console.log(status);
+	if (data.status == 'OK') {
+		$('div#api_status').toggleClass('available')
+	}
+  })
+})
