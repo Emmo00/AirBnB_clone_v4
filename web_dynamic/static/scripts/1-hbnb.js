1a5226ac6a831eda8c2dec7da26ac0951a3810b4
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-08-07 14:00:32 +0100

    finish 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,19 @@
 $(function () {
-  let amenityIds = {};
+  let amenities = {};
 
-  $('input[type="checkbox"]').on('change', function() {
-      const amenityId = $(this).data('amenity-id');
+  $('input[type="checkbox"]').on("change", function () {
+    const amenityId = $(this).data("id");
+    const amenityName = $(this).data("name");
+    let amenityList = "";
 
-      if ($(this).is(':checked')) {
-        amenityIds[amenityId] = true;
-      } else {
-        delete amenityIds[amenityId];
-      }
-      updateAmenityList(amenityIds);
-  })                                                  })
+    if ($(this).is(":checked")) {
+      amenities[amenityId] = amenityName;
+    } else {
+      delete amenities[amenityId];
+    }
+    for (let amenity of amenities) {
+      amenityList += `${amenity}, `
+    }
+    $(".amenities h4").text(amenityList);
+  });
+});
